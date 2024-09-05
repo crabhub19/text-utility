93c002b9c50ac79403284b2713c579a91a5e6e97
Bishal <144455368+crabhub19@users.noreply.github.com>
2024-09-05 23:06:15 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import creator from './creator.png'
 export default function Footer() {
   return (
     <div>
-        <div className='flex justify-center items-center p-1 gap-4 bg-gray-200 navbar dark:bg-gray-900 dark:text-white'>
-            <img className='w-6 rounded-full' src={creator} alt="" srcset="" />
+        <div className='d-flex justify-content-center align-items-center p-1 gap-4 bg-dark'>
+            <img className="rounded-circle" style="width: 1.5rem; height: 1.5rem;" src={creator} alt="" srcset="" />
             <b>Xlorex Bishal</b>
         </div>
     </div>
